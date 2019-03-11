db57a763a8e4271b10726110bd19d4f15519907a
ozgurbariskas <ozgurbariskas@gmail.com>
2019-03-12 02:02:37 +0300

    new

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Özgür Barış KAŞ</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<header>
+    <h1>Özgür Barış KAŞ</h1>
+</header>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php include 'header.php'; ?>
+<main>
+    <fieldset>
+        <legend>Information</legend>
+        
+		<table>
+			<tr>
+				<td><b>Address:</b></td>
+				<td>35340 Narlidere/İzmir(Turkey)</td>
+			</tr>
+			<tr>
+				<td><b>Phone:</b></td>
+				<td> +90542 788 23 36</td>
+			</tr>
+			<tr>
+				<td><b>E-mail:</b></td>
+				<td> ozgurbariskas@gmail.com</td>
+			</tr>
+			<tr>
+				<td><b>Web Site:</b></td>
+				<td> <a href="http://ozgurbariskas.github.io">ozgurbariskas.github.io</a></td>
+			</tr>			
+		</table>
+    </fieldset>
+    <fieldset>
+        <legend>Education</legend>
+        
+        <table>
+			<tr>
+				<td>Ege University, Computer Engineering, Undergraduate, İzmir (Turkey) </td>
+			</tr>
+		</table>
+    </fieldset>
+	<fieldset>
+        <legend>Projects</legend>
+        
+        <table>
+			<tr>
+				<td>Project Name: Androcopter
+					<p>Tournament: TUBITAK 44.th High school tournament, year 2013</p>
+					<p>Award: 2nd of Erzurum Region</p>
+					<p>Project Description: Designed a remote controlled, through android device, drone that can broadcast
+					and record video. Connected the drone with gsm network.</p> </td>
+			</tr>
+		</table>
+    </fieldset>
+</main>
+<?php include 'footer.php'; ?>
